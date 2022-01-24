0b008c007979582a5a35c5f74577fb3479203f3b
PrabhashaOnline <89778323+PrabhashaOnline@users.noreply.github.com>
2022-01-24 18:02:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,19 +24,6 @@ async def start(event):
     "__**I'm MentionAll Bot**, I can mention almost all members in group or channel 👻\nTap **/help** for more information__",
     link_preview=False
 
-@client.on(events.NewMessage(pattern="^/help$"))
-async def help(event):
-  helptext = "**Help Menu of MentionAllBot**\n\nCommand: /mentionall\n__You can use this command with text what you want to mention others.__\n`Example: /mentionall Good Morning!`\n__You can you this command as a reply to any message. Bot will tag users to that replied messsage__.\n\nFollow [@AnjanaMadu](https://github.com/AnjanaMadu) on Github"
-  await event.reply(
-    helptext,
-    link_preview=False,
-    buttons=(
-      [
-        Button.url('📣 Channel', 'https://t.me/harp_tech'),
-        Button.url('📦 Source', 'https://github.com/AnjanaMadu/MentionAllBot')
-      ]
-    )
-  )  
 @client.on(events.NewMessage(pattern="^/all ?(.*)"))
 async def mentionall(event):
   chat_id = event.chat_id
